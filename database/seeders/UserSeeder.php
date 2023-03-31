f90312771e2f20df7e06a7f3d0e97ff18c007c09
wekauwau <wekauwau@gmail.com>
2023-03-31 14:30:42 +0700

    Fix UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,17 +12,144 @@ class UserSeeder extends Seeder
         $data = [
             [
                 'name' => 'Anita Durotul Yatimah',
-                'username' => '',
                 'phone' => '',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             ],
             [
-                'name' => 'Nizar',
-                'username' => 'nizar',
+                'name' => 'Mohammad Nizar',
                 'phone' => '',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             ],
-            // TODO: teruskan
+            [
+                'name' => 'Isna Rahmi N.',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Bagus Faza Mujtaba',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Daffaul Faizah',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'M. Syaiful',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Faat R.',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Handika Yoga Pratama',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Amelia Nur Ma\'ani',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Hasan R.',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Vika N.',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Muhammad Syamsul Ashfia',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Alaiyya Ayu Ni\'matul Fajriya',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Wahyu Eka Saputra',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Annisa Rif\'atul Himmah',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Wahyu Wagiyanto',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Muhammad Taufiqurrahman',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Hidayatul Munaziroh',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Naupal Mubarok',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Nila Sa\'adah',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Nur Muhammad Ikhsanun',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Adhif Mas Ula',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Zulfa Farikhatun Nisak',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Ahmad Irham Wildan F.',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Ulin Nuha Pangesti',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Abdul Rosyidulhaq',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Anis Samchati',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
+            [
+                'name' => 'Husnul Muadiba',
+                'phone' => '',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            ],
         ];
 
         foreach ($data as $val) {
