a6806310596305f89d3ad3edbcd78269f8fa41c9
wekauwau <wekauwau@gmail.com>
2023-04-01 00:41:49 +0700

    Fix Data

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -11,19 +11,19 @@ class DataSeeder extends Seeder
     {
         $data = [
             [
-                'divisions_id' => 6,
+                'divisions_id' => 6,  // Keamanan Pesantren
                 'name' => 'Hasil peraturan keamanan dawuh ibuk 2 Nov 2021',
                 'link' => 'https://docs.google.com/document/d/12-7UiG-4SizZ5aBElaJElf6NvTrfVTzgxZutWqVtcg0/edit?usp=sharing',
                 'file' => null,
             ],
             [
-                'divisions_id' => 14,
+                'divisions_id' => 10,  // Humas Idul Adha 2022
                 'name' => 'Iuran dan Sohibul Kurban 2022',
                 'link' => 'https://docs.google.com/document/d/1RVDLJVe-8PCmMp-dwV-0jyzjwdQD6P-q/edit?usp=sharing&ouid=116339038139849099361&rtpof=true&sd=true',
                 'file' => null,
             ],
             [
-                'divisions_id' => 6,
+                'divisions_id' => 6,  // Keamanan Pesantren
                 'name' => 'Takzir telat pulang liburan 2022',
                 'link' => 'https://docs.google.com/spreadsheets/d/16HkeA55xIdKqrWgfvqC-PTYpctbBg-NhRJqEFvj69TM/edit?amp;usp=embed_facebook#gid=0',
                 'file' => null,
