cd2e63885186500ec516b82d19bc0b6d422b9741
wekauwau <wekauwau@gmail.com>
2024-02-06 21:26:01 +0700

    Add:model Room -mfs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RoomGroupSeeder::class,
+            RoomSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Room;
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Room::factory(10)->create();
+    }
+}
