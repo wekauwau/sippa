13fa83cb22f43dfff13680b72041a2a03c197f57
wekauwau <wekauwau@gmail.com>
2024-02-08 22:52:03 +0700

    chore: add new helper function

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,9 @@ function get_ids($model)
     $ids = $model::pluck('id')->all();
     return fake()->randomElement($ids);
 }
+
+function get_random_user_id($model)
+{
+    $user_ids = $model::pluck('user_id')->all();
+    return fake()->randomElement($user_ids);
+}
