f2d7b21bc88e51de269346a9eb8a10fdcef9d1db
wekauwau <wekauwau@gmail.com>
2024-03-10 13:17:58 +0700

    fix PaymentSeeder

--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Payment;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PaymentSeeder extends Seeder
 {
@@ -16,5 +17,16 @@ class PaymentSeeder extends Seeder
                 'status' => fake()->boolean(),
             ]);
         }
+
+        DB::table('payments')
+            ->where('bill_id', '=', 4)
+            ->where('user_id', '=', 42)
+            ->update(['status' => 0]);
+
+        Payment::create([
+            'bill_id' => 2,
+            'user_id' => 42,
+            'status' => 1,
+        ]);
     }
 }
