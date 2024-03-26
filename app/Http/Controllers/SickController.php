25da07d15126586f353e6a899fa654bca377db93
wekauwau <wekauwau@gmail.com>
2024-03-26 20:44:54 +0700

    add yajra/laravel-datatables dependencies

---
+++ app/Http/Controllers/SickController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DataTables;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SickController extends Controller
+{
+    public function view()
+    {
+        return view('health');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = DB::table('sicks')
+                ->get();
+            error_log('ppp');
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 import './bootstrap';
+import 'bootstrap';
+import '@popperjs/core';
+import 'laravel-datatables-vite';
