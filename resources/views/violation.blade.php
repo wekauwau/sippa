b2cf308849b59800547677169ce2ac5142f6865b
wekauwau <wekauwau@gmail.com>
2024-03-27 21:38:49 +0700

    refactor: page:health, violation
    Add shadow

--- resources/views/violation.blade.php
+++ resources/views/violation.blade.php
@@ -8,13 +8,15 @@
     @endpush
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <x-header>
-                <x-slot:title>
-                    Pelanggaran
-                    </x-slot>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-header>
+                    <x-slot:title>
+                        Pelanggaran
+                        </x-slot>
 
-                    {{ $dataTable->table() }}
-            </x-header>
+                        {{ $dataTable->table() }}
+                </x-header>
+            </div>
         </div>
     </div>
     @push('scripts')
