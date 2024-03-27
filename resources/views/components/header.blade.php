8f3c6fa5692566cf3beb36864972a64f3c224300
wekauwau <wekauwau@gmail.com>
2024-03-27 11:48:34 +0700

    refactor: components/header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+<div class="p-4 lg:p-8 bg-white border-b border-gray-200">
     <h1 class="text-2xl font-medium text-gray-900 mb-10">
         {{ $title }}
     </h1>
