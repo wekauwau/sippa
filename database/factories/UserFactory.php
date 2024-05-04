18ccd836da9fea0cfc5cc1721896eb0f4e32140b
wekauwau <wekauwau@gmail.com>
2024-05-04 17:18:07 +0700

    feature: User add nickname

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'nickname' => '',
             'phone' => fake()->phoneNumber(),
             'username' => fake()->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nickname');
             $table->string('phone');
             $table->string('username');
             $table->string('password');
