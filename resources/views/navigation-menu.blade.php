eb360ea32507adf7c5b6a8e68f1292369d392f19
wekauwau <wekauwau@gmail.com>
2024-05-04 10:23:47 +0700

    feature: remove API

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -119,12 +119,6 @@
                                 Profil
                             </x-dropdown-link>
 
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                {{ __('API Tokens') }}
-                            </x-dropdown-link>
-                            @endif
-
                             <div class="border-t border-gray-200"></div>
 
                             <!-- Authentication -->
@@ -203,12 +197,6 @@
                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
-
-                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
-                    {{ __('API Tokens') }}
-                </x-responsive-nav-link>
-                @endif
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
