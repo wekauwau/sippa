8f151779bc0c822b27c236c51f4b3eca390ec62d
wekauwau <wekauwau@gmail.com>
2024-05-07 13:17:35 +0700

    (UserFactory) add temp faker->username to users.nickname

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,10 @@ class UserFactory extends Factory
 {
     public function definition(): array
     {
+        // TODO: remove temp nickname = faker->username
         return [
             'name' => fake()->name(),
-            'nickname' => '',
+            'nickname' => fake()->userName(),
             'phone' => fake()->phoneNumber(),
             'username' => fake()->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
