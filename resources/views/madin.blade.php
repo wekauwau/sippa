49cfe60fe89b13c937296c8cd26e6c54b1800940
wekauwau <wekauwau@gmail.com>
2024-05-10 09:41:53 +0700

    only show Jadwal Mengajar Saya if user is teacher

--- resources/views/madin.blade.php
+++ resources/views/madin.blade.php
@@ -2,6 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @if(Auth::user()->teacher)
                 <x-header>
                     <x-slot:title>
                         Jadwal Mengajar Saya
@@ -9,6 +10,7 @@
 
                         <x-my-madin-schedule :userid="Auth::id()" />
                 </x-header>
+                @endif
 
                 <x-header>
                     <x-slot:title>
