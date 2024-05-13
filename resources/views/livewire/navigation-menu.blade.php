d955a225f60406722acc18e96e662f944411c583
wekauwau <wekauwau@gmail.com>
2024-05-13 18:56:22 +0700

    fix nav responsive for xs, sm, lg

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -11,24 +11,44 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 md:-my-px md:ms-10 md:flex">
+                <div class="hidden space-x-4 lg:space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         Beranda
                     </x-nav-link>
-                    <!-- If authenticated -->
-                    @if (Auth::user())
-                    <x-nav-link href="{{ route('madin') }}" :active="request()->routeIs('madin')">
+                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('madin') }}" :active="request()->routeIs('madin')">
                         Madin
                     </x-nav-link>
-                    <x-nav-link href="{{ route('finance') }}" :active="request()->routeIs('finance')">
+                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('finance') }}" :active="request()->routeIs('finance')">
                         Keuangan
                     </x-nav-link>
-                    <x-nav-link href="{{ route('health') }}" :active="request()->routeIs('health')">
+                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('health') }}" :active="request()->routeIs('health')">
                         Kesehatan
                     </x-nav-link>
-                    <x-nav-link href="{{ route('violation') }}" :active="request()->routeIs('violation')">
+                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('violation') }}" :active="request()->routeIs('violation')">
                         Pelanggaran
                     </x-nav-link>
+                    <x-nav-dropdown class="text-left hidden sm:inline-flex lg:hidden">
+                        <x-slot name="trigger">
+                            Dropdown
+                        </x-slot>
+                        <x-slot name="content">
+                            <x-dropdown-link href="{{ route('madin') }}">
+                                Madin
+                            </x-dropdown-link>
+                            <x-dropdown-link href="{{ route('finance') }}">
+                                Keuangan
+                            </x-dropdown-link>
+                            <x-dropdown-link href="{{ route('health') }}">
+                                Kesehatan
+                            </x-dropdown-link>
+                            <x-dropdown-link href="{{ route('violation') }}">
+                                Pelanggaran
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-nav-dropdown>
+
+                    <!-- If authenticated -->
+                    @if (Auth::user())
                     <!-- Is a manager -->
                     @if ($division_name)
                     <x-nav-link href="{{ route('secretary') }}" :active="request()->routeIs('secretary')">
@@ -39,7 +59,7 @@
                 </div>
             </div>
 
-            <div class="hidden md:flex md:items-center md:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                     <x-dropdown align="right" width="48">
@@ -97,7 +117,7 @@
             </div>
             @endif
 
-            <div class=" -me-2 flex items-center md:hidden">
+            <div class=" -me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -109,7 +129,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden md:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 Beranda
