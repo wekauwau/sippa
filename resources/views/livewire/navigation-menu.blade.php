05d11e9750ae0217e0cda2142a5f9668dc3aa871
wekauwau <wekauwau@gmail.com>
2024-05-23 10:08:13 +0700

    feat: only show required nav links for guest

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -15,46 +15,45 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         Beranda
                     </x-nav-link>
-                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('madin') }}" :active="request()->routeIs('madin')">
-                        Madin
-                    </x-nav-link>
-                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('finance') }}" :active="request()->routeIs('finance')">
-                        Keuangan
-                    </x-nav-link>
-                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('health') }}" :active="request()->routeIs('health')">
-                        Kesehatan
-                    </x-nav-link>
-                    <x-nav-link class="hidden lg:inline-flex" href="{{ route('violation') }}" :active="request()->routeIs('violation')">
-                        Pelanggaran
-                    </x-nav-link>
-                    <x-nav-dropdown class="text-left hidden sm:inline-flex lg:hidden">
-                        <x-slot name="trigger">
-                            Dropdown
-                        </x-slot>
-                        <x-slot name="content">
-                            <x-dropdown-link href="{{ route('madin') }}">
-                                Madin
-                            </x-dropdown-link>
-                            <x-dropdown-link href="{{ route('finance') }}">
-                                Keuangan
-                            </x-dropdown-link>
-                            <x-dropdown-link href="{{ route('health') }}">
-                                Kesehatan
-                            </x-dropdown-link>
-                            <x-dropdown-link href="{{ route('violation') }}">
-                                Pelanggaran
-                            </x-dropdown-link>
-                        </x-slot>
-                    </x-nav-dropdown>
+                    @if (Auth::check())
+                        <x-nav-link class="hidden lg:inline-flex" href="{{ route('madin') }}" :active="request()->routeIs('madin')">
+                            Madin
+                        </x-nav-link>
+                        <x-nav-link class="hidden lg:inline-flex" href="{{ route('finance') }}" :active="request()->routeIs('finance')">
+                            Keuangan
+                        </x-nav-link>
+                        <x-nav-link class="hidden lg:inline-flex" href="{{ route('health') }}" :active="request()->routeIs('health')">
+                            Kesehatan
+                        </x-nav-link>
+                        <x-nav-link class="hidden lg:inline-flex" href="{{ route('violation') }}" :active="request()->routeIs('violation')">
+                            Pelanggaran
+                        </x-nav-link>
+                        <x-nav-dropdown class="text-left hidden sm:inline-flex lg:hidden">
+                            <x-slot name="trigger">
+                                Dropdown
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-dropdown-link href="{{ route('madin') }}">
+                                    Madin
+                                </x-dropdown-link>
+                                <x-dropdown-link href="{{ route('finance') }}">
+                                    Keuangan
+                                </x-dropdown-link>
+                                <x-dropdown-link href="{{ route('health') }}">
+                                    Kesehatan
+                                </x-dropdown-link>
+                                <x-dropdown-link href="{{ route('violation') }}">
+                                    Pelanggaran
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-nav-dropdown>
 
-                    <!-- If authenticated -->
-                    @if (Auth::user())
-                    <!-- Is a manager -->
-                    @if ($division_name)
-                    <x-nav-link href="{{ route('student-data') }}" :active="request()->routeIs('student-data')">
-                        Data Santri
-                    </x-nav-link>
-                    @endif
+                        <!-- Is a manager -->
+                        @if ($division_name)
+                            <x-nav-link href="{{ route('student-data') }}" :active="request()->routeIs('student-data')">
+                                Data Santri
+                            </x-nav-link>
+                        @endif
                     @endif
                 </div>
             </div>
@@ -65,21 +64,27 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                            </button>
-                            @else
-                            @if (Auth::check())
-                            <span class="inline-flex rounded-md">
-                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                    {{ Auth::user()->name }}
-
-                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
+                                <button
+                                    class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                    <img class="h-8 w-8 rounded-full object-cover"
+                                        src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
-                            </span>
-                            @endif
+                            @else
+                                @if (Auth::check())
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
+
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
                             @endif
                         </x-slot>
 
@@ -110,18 +115,23 @@
 
             <!-- Not authenticated -->
             @if (!Auth::check())
-            <div class="flex items-center">
-                <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-1 px-4 rounded">
-                    Masuk
-                </a>
-            </div>
+                <div class="flex items-center">
+                    <a href="{{ route('login') }}"
+                        class="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-1 px-4 rounded">
+                        Masuk
+                    </a>
+                </div>
             @endif
 
             <div class=" -me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -129,7 +139,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 Beranda
@@ -149,37 +159,38 @@
         </div>
 
         @if (Auth::check())
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="flex items-center px-4">
-                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                <div class="shrink-0 me-3">
-                    <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="flex items-center px-4">
+                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                        <div class="shrink-0 me-3">
+                            <img class="h-10 w-10 rounded-full object-cover"
+                                src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        </div>
+                    @endif
+
+                    <div>
+                        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    </div>
                 </div>
-                @endif
 
-                <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
+                <div class="mt-3 space-y-1">
+                    <!-- Account Management -->
+                    <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                        {{ __('Profile') }}
                     </x-responsive-nav-link>
-                </form>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}" x-data>
+                        @csrf
+
+                        <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
             </div>
-        </div>
         @endif
     </div>
 </nav>
