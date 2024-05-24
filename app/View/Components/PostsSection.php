5326b24e02dfbf837b9a1dc0e468531a37f387c4
wekauwau <wekauwau@gmail.com>
2024-05-25 00:04:53 +0700

    feature: integrate Post to dashboard

---
+++ app/View/Components/PostsSection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Post;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\Component;
+
+class PostsSection extends Component
+{
+    public Post $latest_post;
+    public Collection $posts;
+
+    public function __construct()
+    {
+        $records = Post::orderBy('id', 'desc')
+            ->take(4)
+            ->get();
+
+        $this->latest_post = $records->shift();
+        $this->posts = $records;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.posts-section');
+    }
+}
---
+++ resources/views/components/posts-section.blade.php
@@ -0,0 +1,45 @@
+<section class="dark:bg-gray-800 dark:text-gray-100">
+    <div class="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+        <a rel="noopener noreferrer" href="#"
+            class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+            <img src="images/{{ $latest_post->image }}" alt=""
+                class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500">
+            <div class="p-6 space-y-2 lg:col-span-5">
+                <h3 class="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
+                    {{ $latest_post->title }}
+                </h3>
+                <span class="text-xs dark:text-gray-400">
+                    {{ $latest_post->published_at }}
+                </span>
+                <p class="line-clamp-4 overflow-ellipsis">
+                    {{ $latest_post->content }}
+                </p>
+            </div>
+        </a>
+        <div class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            @foreach ($posts as $post)
+                <a rel="noopener noreferrer" href="#"
+                    class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                    <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500"
+                        src="images/{{ $post->image }}">
+                    <div class="p-6 space-y-2">
+                        <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">
+                            {{ $post->title }}
+                        </h3>
+                        <span class="text-xs dark:text-gray-400">
+                            {{ $post->published_at }}
+                        </span>
+                        <p class="line-clamp-3 overflow-ellipsis">
+                            {{ $post->content }}
+                        </p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+        <div class="flex justify-center">
+            <a href="{{ route('blogs') }}"
+                class="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-900 dark:text-gray-400">Lihat lebih
+                banyak</a>
+        </div>
+    </div>
+</section>
