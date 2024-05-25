31c9a63975f297085e48498cc5af207c456bbcc3
wekauwau <wekauwau@gmail.com>
2024-05-25 20:34:54 +0700

    refactor(route): profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,9 +10,9 @@
 
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.update-password-form')
-            </div>
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.update-password-form')
+                </div>
             @endif
         </div>
     </div>
