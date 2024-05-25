549ec0ff4a1d5306d4c42cfe623ce95946690d93
wekauwau <wekauwau@gmail.com>
2024-05-25 15:28:15 +0700

    seeder: add more posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -11,6 +11,123 @@ class PostSeeder extends Seeder
     {
         $records = [
             [
+                'poster-alba-berselawat.jpg',
+                'Alba Bersholawat',
+                'Berikut kami informasikan himbauan dan peringatan khusus Al-Barokah Bersholawat, terutama kepada seluruh jamaah dan pasukan pengibar bendera besar.
+
+Mohon dibaca dengan cermat. Tanpa mengurangi rasa hormat, kami harap peraturan ini dilaksanakan sebagaimana mestinya. 🙏
+
+💠Aktualisasi Nilai-Nilai Al-Qur\'an untuk Menjadi Generasi Islami Terdepan💠
+
+Nantikan informasi terbaru seputar Haflah Tasyakur Khotmil Qur\'an XXIV dan Haul al-Maghfurlah KH. Rosim Al-Fatih, LC. Ke-V melalui kanal informasi media sosial kami. Jangan lupa jaga kesehatan!',
+            ],
+            [
+                'alba-berselawat.jpg',
+                'Dokumentasi Alba Bersholawat',
+                'Sholawat bersama Habib Alwi Zahir Bin Mohammad Al-Habsyi dan Ahbabul Musthofa Yogyakarta dalam rangka Haflah Tasyakur Khotmil Qur\'an XXIV dan Haul Al-maghfurlah K.H. Rosim Al-Fatih, Lc Ke-5.
+
+Rangkaian malam puncak sholawat Al-barokah telah terlaksana dengan luar biasa meriah, dipenuhi oleh pecinta Nabi yang datang dari segala arah, membawa tujuan yang sama, yakni mengalap berkah dan cinta Nabi Muhammad SAW.
+
+Semoga dengan terlaksananya kegiatan ini, kita senantiasa istiqomah dalam menjunjung tinggi nilai-nilai teladan sang Baginda Nabi.
+
+Tak pandang sedikit atau banyaknya kuantitas yang diberikan, terima kasih atas partisipasi yang luar biasa hingga acara berjalan sempurna.
+
+💠Aktualisasi Nilai-Nilai Al-Qur\'an untuk Menjadi Generasi Islami Terdepan💠
+
+Jangan lupa jaga kesehatan!',
+            ],
+            [
+                'isra-miraj.jpg',
+                'Malam peringatan Isra Mi\'raj',
+                'Malam peringatan Isro mi\'raj di Pondok Pesantren Al barokah 2024 diisi dengan pembacaan Maulid Diba\' dan Mujahadah rutin.
+
+Isro merupakan peristiwa ketika Allah memperjalankan Nabi Muhammad dari Masjidil Haram menuju Masjidil Aqsha, sementara Mi\'raj merupakan dinaikkannya Nabi Muhammad menuju Sidratul Muntaha.
+
+Peristiwa bersejarah ini diabadikan dalam firman Allah swt Surat Al-Isra ayat 1:
+
+سُبۡحَٰنَ ٱلَّذِيٓ أَسۡرَىٰ بِعَبۡدِهِۦ لَيۡلٗا مِّنَ ٱلۡمَسۡجِدِ ٱلۡحَرَامِ إِلَى
+ٱلۡمَسۡجِدِ ٱلۡأَقۡصَا ٱلَّذِي بَٰرَكۡنَا حَوۡلَهُۥ لِنُرِيَهُۥ مِنۡ ءَايَٰتِنَآۚ إِنَّهُۥ هُوَ ٱلسَّمِيعُ ٱلۡبَصِيرُ
+
+Sebelum Nabi menghadap ke Allah SWT. Nabi di bersihkan terlebih dahulu hatinya agar hati yang suci semakin menjadi suci. Pembersihan ini dilakukan sebelum Nabi mendapat perintah sholat lima waktu.
+
+Dari kisah ini dapat di jadikan pelajaran bagi kita umat Nabi Muhammad SAW. Hendaknya sebelum melaksanakan sholat lima waktu kita juga bersiap dengan hati yang bersih, tidak hanya tempat atau pakaian saja yang diperhatikan. Hal ini sebagai bentuk ikhtiar supaya sholat yang kita lakukan bisa benar-benar khusyu\'.
+
+Semoga dengan memperbanyak bacaan sholawat. Kita semua mendapat syafaat dari Rasulullah SAW di hari akhir kelak. Aamiin Ya Rabbal \'alamin
+
+Kamis, 8 February 2024
+Aula Pondok Pesantren Al Barokah',
+            ],
+            [
+                'ziarah.jpg',
+                'Ziarah Al-Barokah',
+                'Assalamu\'alaikum warahmatullahi wabarakatuh
+
+Ikutilah Ziarah bersama Al Barokah terbuka untuk UMUM (kuota terbatas!) yang akan dilaksanakan pada:
+
+🗓 Tanggal: 1-3 Maret 2024/ 20-22 Sya\'ban 1445 H
+⏰ Berangkat: Jumat, 1 Maret 2024 ba\'da isya, Pulang: Minggu, 3 Maret 2024 Pukul 04.00
+📍Titik kumpul: Aula Al Barokah
+🛣 Tujuan: Jawa Barat dan Jawa Tengah (rincian destinasi terlampir di poster)
+💸 Biaya: Rp 330.000
+
+📌 Batas akhir konfirmasi Ziarah 26 Februari 2024
+📌 Batas akhir pembayaran Ziarah 27 Februari 2024
+
+💵Pembayaran secara non tunai dapat dilakukan melalui:
+642201018861533 (BRI) a.n. Luthfi Azizatul Ulya
+90140211289 (BTPN) a.n. Almas Lathifatul Ula
+1219050990 (BNI) a.n. Husnul Khotimah
+
+📝Konfirmasi pendaftaran dan pembayaran dengan menghubungi
+wa.me/+6285732672082 (Luthfi Azizatul U).
+
+Sekian pengumuman yang dapat kami sampaikan, jika ada pertanyaan silakan bisa ditanyakan kepada Panitia Ziarah. Terima kasih. 😇
+
+"Ziarah Alba! Ngalap Berkah Sareng Al Barokah"
+#ziarahalba2024',
+            ],
+            [
+                'nuzulul-quran.jpg',
+                'Nuzulul Qur\'an',
+                '[PERINGATAN NUZULUL QUR\'AN 1445 H]
+
+Nuzulul Qur\'an merupakan momen yang paling penting bagi umat Islam, karena di dalamnya Kalam Allah (Al-Qur\'an) telah turun dengan pertama kalinya di hari ke 17 Ramadhan kepada Baginda Nabi Muhammad Saw.
+Sebagian para ulama sepakat bahwa turunnya Al-Qur\'an secara bertahap dari Lauhul Mahfudz lalu sekaligus diturunkan ke Baitu \'izzah sampai dengan seterusnya sedikit demi sedikit diturunkan oleh malaikat Jibril finalnya hingga kepada Nabi Muhammad Saw.
+
+"Dan Al-Qur\'an itu telah kami turunkan berangsur-angsur agar kamu membacakannya perlahan-lahan kepada manusia dan menurunkannya bagian demi bagian. (Q.S. Al-Isro: 106)
+
+Dalam rangka memperingati Nuzulul Qur\'an, Pondok pesantren Al-Barokah mengadakan acara peringatan dengan rincian:
+
+Tanggal: 28 Maret 2024
+Waktu: 15.30
+Tempat: Aula Pondok Pesantren Al-Barokah
+Pemateri: Bpk . Kyai Ahmad Sahal
+Tema: Menggali Lebih Dalam Makna Nuzulul Qur\'an',
+            ],
+            [
+                'lailatul-qadar.jpg',
+                'Keutamaan Lailatul Qadar',
+                '[LAILATUL QADAR; RAMADHAN 1445H]
+
+Selangkah lebih bijak, untuk Ramadhan yang tinggal setapak. Ramadhan terasa cepat berlalu, tidak terkira kita bakal akan meninggalkan bulan ini dengan tinggal sepertiganya. Sepuluh hari terakhir ini jangan disia-siakan rugi dong kalau dilewatkan begitu saja. Karena ia merupakan puncak untuk berdoa dan berdialog bathin dengan Yang Rahman, puncak dari perjalanan peribadahan kita selama bulan Ramadhan ini, yang memiliki keistimewaan, keagungan yang tidak ditemukan pada bulan-bulan yang lain.
+
+اِنَّآ اَنْزَلْنٰهُ فِيْ لَيْلَةِ الْقَدْرِ ١. وَمَآ اَدْرٰىكَ مَا لَيْلَةُ الْقَدْرِۗ. ٢. لَيْلَةُ الْقَدْرِ ەۙ خَيْرٌ مِّنْ اَلْفِ شَهْرٍۗ. ٣. تَنَزَّلُ الْمَلٰۤىِٕكَةُ وَالرُّوْحُ فِيْهَا بِاِذْنِ رَبِّهِمْۚ مِنْ كُلِّ اَمْرٍۛ. ٤.سَلٰمٌ ۛهِيَ حَتّٰى مَطْلَعِ الْفَجْرِ ࣖ٥.
+
+Semoga Allah ta\'ala berkenan menerima taubat dan istighfar, menerima amal yang kita kerjakan dan mengabulkan doa-doa kita, mengantarkan kita pada perubahan yang baik untuk kedepannya.
+
+اللَّهُمَّ بَارِك لَنَا فِي شَهْرِ رَمَضَانَ وَ بَلِغْنَا لَيلَة القَدرِ وَعِبَدَةً فِيهَا.
+"Ya Allah, berkahilah kami di bulan Ramadhan ini dan pertemukan kami dengan Lailatul Qadar dan beribadah di malamnya."
+
+Di penghujung bulan ini, mari gunakan waktu sebaik-baiknya. Manfaatkan untuk bertaqarub dengan Allah SWT dan mencari keridhoan Allah SWT. Tidak ketinggalan juga, di Pondok pesantren Al-Barokah biasanya mengadakan sholat qiyamul lail yang merupakan rutinan pondok sehingga semoga santri-santri turut andil menunaikan ibadah terutama pada sepertiga malam, malam-malam terakhir Ramadhan. Aamiin...',
+            ],
+            [
+                'syawalan.jpg',
+                'Dokumentasi Syawalan Santri Alba',
+                'Idul Fitri telah tiba, waktunya syawalan bersama Santri AlBA~~
+
+"Tidaklah sedekah itu mengurangi harta, dan tidaklah Allah menambah bagi seorang hamba dengan pemberian maafnya (kepada saudaranya) kecuali kemuliaan (di dunia dan akhirat), serta tidaklah seseorang merendahkan diri karena Allah kecuali Dia akan meninggikan (derajat)nya (di dunia dan akhirat)." (HR Muslim)',
+            ],
+            [
                 'post-qurban.jpg',
                 'Berbagi Qurban Bersama Pondok Pesantren Al-Barokah',
                 'Assalamualaikum Wr. Wb
@@ -36,23 +153,14 @@ Informasi lebih lanjut dapat menghubungi nomor berikut:
 
 Atas perhatian dan partisipasi kami sampaikan terima kasih.😁
 Wassalamualaikum Wr.Wb.',
-            ]
+            ],
         ];
 
-        // foreach ($records as $record) {
-        //     Post::create([
-        //         'image' => $record[0],
-        //         'title' => $record[1],
-        //         'content' => $record[2],
-        //     ]);
-        // }
-
-        // temporary seeder
-        for ($i = 0; $i < 10; $i++) {
+        foreach ($records as $record) {
             Post::create([
-                'image' => $records[0][0],
-                'title' => $records[0][1],
-                'content' => $records[0][2],
+                'image' => $record[0],
+                'title' => $record[1],
+                'content' => $record[2],
             ]);
         }
     }
