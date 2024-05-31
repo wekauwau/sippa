04e02fafb04a1e4e86c03be5be9bc6973c09eb67
wekauwau <wekauwau@gmail.com>
2024-05-31 18:52:52 +0700

    fix(/warta): gallery sidebar reload every page change

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,7 +2,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:posts />
+                <div class="container mx-auto flex flex-wrap py-6">
+                    <!-- Posts Section -->
+                    <livewire:posts />
+
+                    <!-- Sidebar Section -->
+                    <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+                        <x-gallery />
+                    </aside>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,37 +1,28 @@
-<div class="container mx-auto flex flex-wrap py-6">
-    <!-- Posts Section -->
-    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+<section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
-        @foreach ($posts as $post)
-            <article class="flex flex-col shadow my-4">
-                <!-- Article Image -->
-                <a href="{{ route('post', [$post->id]) }}" class="h-96 overflow-y-hidden hover:opacity-75">
-                    <img src="{{ route('image', [$post->image]) }}">
+    @foreach ($posts as $post)
+        <article class="flex flex-col shadow my-4">
+            <!-- Article Image -->
+            <a href="{{ route('post', [$post->id]) }}" class="h-96 overflow-y-hidden hover:opacity-75">
+                <img src="{{ route('image', [$post->image]) }}">
+            </a>
+            <div class="bg-white flex flex-col justify-start p-6">
+                <a href="{{ route('post', [$post->id]) }}" class="text-3xl font-bold hover:text-gray-700 pb-4">
+                    {{ $post->title }}
                 </a>
-                <div class="bg-white flex flex-col justify-start p-6">
-                    <a href="{{ route('post', [$post->id]) }}" class="text-3xl font-bold hover:text-gray-700 pb-4">
-                        {{ $post->title }}
-                    </a>
-                    <p class="text-sm pb-3">
-                        {{ $post->published_at }}
-                    </p>
-                    <p class="mb-6 overflow-hidden line-clamp-3 overflow-ellipsis">
-                        {{ $post->content }}
-                    </p>
-                    <a href="{{ route('post', [$post->id]) }}" class="uppercase text-gray-800 hover:text-black">
-                        lanjutkan membaca <i class="fas fa-arrow-right"></i>
-                    </a>
-                </div>
-            </article>
-        @endforeach
+                <p class="text-sm pb-3">
+                    {{ $post->published_at }}
+                </p>
+                <p class="mb-6 overflow-hidden line-clamp-3 overflow-ellipsis">
+                    {{ $post->content }}
+                </p>
+                <a href="{{ route('post', [$post->id]) }}" class="uppercase text-gray-800 hover:text-black">
+                    lanjutkan membaca <i class="fas fa-arrow-right"></i>
+                </a>
+            </div>
+        </article>
+    @endforeach
 
-        {{ $posts->links() }}
+    {{ $posts->links() }}
 
-    </section>
-
-    <!-- Sidebar Section -->
-    <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
-        <x-gallery />
-    </aside>
-
-</div>
+</section>
