0165ac5bc215702426d7c449cfa11d40d4fa764a
wekauwau <wekauwau@gmail.com>
2024-06-05 19:57:25 +0700

    fix(BillSeeder): match real data

--- database/seeders/BillSeeder.php
+++ database/seeders/BillSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Bill;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class BillSeeder extends Seeder
 {
@@ -18,10 +17,22 @@ class BillSeeder extends Seeder
                 '300.000',
             ],
             [
+                'Syahriah bulan Januari (potongan)',
+                null,
+                date_create('2024-01-10'),
+                '200.000',
+            ],
+            [
                 'Syahriah bulan Februari',
                 null,
                 date_create('2024-02-10'),
                 '300.000',
+            ],
+            [
+                'Syahriah bulan Februari (potongan)',
+                null,
+                date_create('2024-02-10'),
+                '200.000',
             ],
             [
                 'Ziarah',
@@ -30,16 +41,58 @@ class BillSeeder extends Seeder
                 '310.000',
             ],
             [
+                'Syahriah bulan Maret',
+                null,
+                date_create('2024-03-10'),
+                '300.000',
+            ],
+            [
+                'Syahriah bulan Maret (potongan)',
+                null,
+                date_create('2024-03-10'),
+                '200.000',
+            ],
+            [
                 'Iuran Ramadhan',
                 null,
                 date_create('2024-03-06'),
                 '110.000',
             ],
             [
-                'Syahriah bulan Maret',
+                'Syahriah bulan April',
                 null,
-                date_create('2024-03-10'),
+                date_create('2024-04-10'),
                 '300.000',
+            ],
+            [
+                'Syahriah bulan April (potongan)',
+                null,
+                date_create('2024-04-10'),
+                '200.000',
+            ],
+            [
+                'Syahriah bulan Mei',
+                null,
+                date_create('2024-05-10'),
+                '300.000',
+            ],
+            [
+                'Syahriah bulan Mei (potongan)',
+                null,
+                date_create('2024-05-10'),
+                '200.000',
+            ],
+            [
+                'Syahriah bulan Juni',
+                null,
+                date_create('2024-06-10'),
+                '300.000',
+            ],
+            [
+                'Syahriah bulan Juni (potongan)',
+                null,
+                date_create('2024-06-10'),
+                '200.000',
             ],
         ];
 
@@ -52,20 +105,38 @@ class BillSeeder extends Seeder
             ]);
         }
 
-        for ($i = 1; $i <= 5; $i++) {
-            DB::table('bills')
-                ->where('id', $i)
-                ->update(['created_at' => "2024-0{$i}-01 00:00:00"]);
+        // created_at at 1 each month for Syahriah
+        $counter = 1;
+        foreach ([
+            [1, 2],
+            [3, 4],
+            [6, 7],
+            [9, 10],
+            [11, 12],
+            [13, 14],
+        ] as $ids) {
+            foreach ($ids as $id) {
+                Bill::where('id', $id)
+                    ->update([
+                        'created_at' => date_create("2024-{$counter}-01"),
+                        'updated_at' => date_create("2024-{$counter}-01"),
+                    ]);
+            }
+            $counter++;
         }
 
-        DB::table('bills')
-            ->where('id', 3)
-            ->update(['created_at' => '2024-02-20 00:00:00']);
-        DB::table('bills')
-            ->where('id', 4)
-            ->update(['created_at' => '2024-02-25 00:00:00']);
-        DB::table('bills')
-            ->where('id', 5)
-            ->update(['created_at' => '2024-03-01 00:00:00']);
+        // Ziarah
+        Bill::where('id', 5)
+            ->update([
+                'created_at' => date_create("2024-02-01"),
+                'updated_at' => date_create("2024-02-01"),
+            ]);
+
+        // Iuran ramadhan
+        Bill::where('id', 8)
+            ->update([
+                'created_at' => date_create("2024-03-01"),
+                'updated_at' => date_create("2024-03-01"),
+            ]);
     }
 }
