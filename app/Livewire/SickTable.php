c046c9e9237bbfec52996670020894733d93de17
wekauwau <wekauwau@gmail.com>
2024-06-11 22:09:41 +0700

    refactor

--- app/Livewire/SickTable.php
+++ app/Livewire/SickTable.php
@@ -3,9 +3,9 @@
 namespace App\Livewire;
 
 use App\Models\Sick;
+use App\Traits\SickTable\Columns;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
@@ -18,26 +18,15 @@ class SickTable extends Component implements HasTable, HasForms
     use InteractsWithTable;
     use InteractsWithForms;
 
+    use Columns;
+
     public function table(Table $table): Table
     {
         $query = Sick::where('student_id', Auth::user()->student->id);
 
         return $table
             ->query($query)
-            ->columns([
-                TextColumn::make('start')
-                    ->label('Mulai')
-                    ->sortable()
-                    ->searchable(isIndividual: true),
-                TextColumn::make('end')
-                    ->label('Sembuh')
-                    ->sortable()
-                    ->searchable(isIndividual: true),
-                TextColumn::make('info')
-                    ->label('Keterangan')
-                    ->sortable()
-                    ->searchable(isIndividual: true),
-            ]);
+            ->columns($this->getColumns());
     }
 
     public function render(): View
