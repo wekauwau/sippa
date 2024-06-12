d66e3281741fb0d225c0c3b438271c26c88f06bf
wekauwau <wekauwau@gmail.com>
2024-06-12 12:35:24 +0700

    feat(route:violation-data): add C header actions

--- app/Traits/FilamentTable/HasDifferentColumnForManager.php
+++ app/Traits/FilamentTable/HasDifferentColumnForManager.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits\FilamentTable;
 
+use App\Models\Student;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -41,6 +43,40 @@ trait HasDifferentColumnForManager
                     isIndividual: true,
                 ),
             ...$this->getColumns(),
+        ];
+    }
+
+    private function addSelectStudent(): array
+    {
+        $student_options = Student::whereRelation(
+            'user',
+            'active',
+            1
+        )->get()->pluck('name_with_room', 'id');
+
+        return [
+            Select::make('student_id')
+                ->label("Santri")
+                ->required()
+                ->native(false)
+                ->options($student_options)
+                ->searchable()
+                ->getSearchResultsUsing(
+                    function (string $search) use ($student_options): array {
+                        if ($search == '') return $student_options;
+                        return $student_options
+                            ->filter(function ($item) use ($search) {
+                                return false !== stripos($item, $search);
+                            })
+                            ->toArray();
+                    }
+                )
+                ->getOptionLabelUsing(
+                    function ($value) use ($student_options): string {
+                        return $student_options[$value];
+                    }
+                ),
+            ...$this->getFormEdit(),
         ];
     }
 }
--- app/Traits/ViolationTable/Actions.php
+++ app/Traits/ViolationTable/Actions.php
@@ -2,11 +2,22 @@
 
 namespace App\Traits\ViolationTable;
 
+use App\Traits\FilamentTable\HasDifferentColumnForManager;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\CreateAction;
+
 trait Actions
 {
+    use HasDifferentColumnForManager;
+
     private function getHeaderActions(): array
     {
-        return [];
+        return [
+            CreateAction::make()
+                ->label("Tambah")
+                ->form($this->getFormCreate()),
+        ];
     }
 
     private function getBulkActions(): array
@@ -17,5 +28,30 @@ trait Actions
     private function getActions(): array
     {
         return [];
+    }
+
+    private function getFormCreate(): array
+    {
+        return $this->addSelectStudent();
+    }
+
+    private function getFormEdit(): array
+    {
+        return [
+            DatePicker::make('date')
+                ->label("Tanggal")
+                ->required()
+                ->native(false)
+                ->format('Y-m-d')
+                ->displayFormat('Y-m-d')
+                ->closeOnDateSelection()
+                ->default(today())
+                ->maxDate(today()),
+            TextInput::make('info')
+                ->label("Keterangan")
+                ->minLength(1)
+                ->maxLength(255)
+                ->required(),
+        ];
     }
 }
