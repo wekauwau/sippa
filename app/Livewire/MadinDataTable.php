e9e88ececa1503615dd7df3ed23eaa2c536fad3b
wekauwau <wekauwau@gmail.com>
2024-06-15 00:11:12 +0700

    feat(MadinDataTable): add C header action

--- app/Livewire/MadinDataTable.php
+++ app/Livewire/MadinDataTable.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use App\Models\Madin;
+use App\Traits\CheckUser;
+use App\Traits\MadinDataTable\Actions;
 use App\Traits\MadinDataTable\Columns;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
@@ -10,6 +12,7 @@ use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class MadinDataTable extends Component implements HasTable, HasForms
@@ -17,10 +20,19 @@ class MadinDataTable extends Component implements HasTable, HasForms
     use InteractsWithTable;
     use InteractsWithForms;
 
+    use CheckUser;
+
+    use Actions;
     use Columns;
 
     public function table(Table $table): Table
     {
+        if ($this->getDivisionName(Auth::user()) == "Pendidikan") {
+            $table->headerActions($this->getHeaderActions())
+                ->bulkActions($this->getBulkActions())
+                ->actions($this->getActions());
+        }
+
         return $table
             ->query(Madin::query())
             ->columns($this->getColumns())
---
+++ app/Traits/MadinDataTable/Actions.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Traits\MadinDataTable;
+
+use App\Models\Semester;
+use App\Models\Teacher;
+use App\Traits\FilamentTable\HasDifferentColumnForManager;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\CreateAction;
+
+trait Actions
+{
+    use HasDifferentColumnForManager;
+
+    private function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label("Tambah")
+                ->modalHeading("Tambah Data Madin")
+                ->form($this->getFormCreate()),
+        ];
+    }
+
+    private function getBulkActions(): array
+    {
+        return [];
+    }
+
+    private function getActions(): array
+    {
+        return [];
+    }
+
+    private function getFormCreate(): array
+    {
+        $semester_options = Semester::all()->pluck('full_name', 'id');
+        $teacher_options = Teacher::all()->pluck('user.name', 'id');
+
+        return [
+            Select::make('semester_id')->label("Semester")
+                ->required()
+                ->native(false)
+                ->options($semester_options)
+                ->searchable()
+                ->getSearchResultsUsing(
+                    function (string $search) use ($semester_options): array {
+                        if ($search == '') return $semester_options;
+                        return $semester_options
+                            ->filter(function ($item) use ($search) {
+                                return false !== stripos($item, $search);
+                            })
+                            ->toArray();
+                    }
+                )
+                ->getOptionLabelUsing(
+                    function ($value) use ($semester_options): string {
+                        return $semester_options[$value];
+                    }
+                ),
+            Select::make('day')->label("Hari")
+                ->required()
+                ->options([
+                    'Ahad' => "Ahad", 'Senin' => "Senin", 'Selasa' => "Selasa", 'Rabu' => "Rabu", 'Jumat' => "Jumat", 'Sabtu' => "Sabtu",
+                ])
+                ->native(false)
+                ->searchable(),
+            Select::make('grade_id')->label("Kelas")
+                ->required()
+                ->relationship(name: 'grade', titleAttribute: 'name')
+                ->preload()
+                ->native(false)
+                ->searchable(),
+            Select::make('madin_room_id')->label("Tempat")
+                ->required()
+                ->relationship(name: 'madin_room', titleAttribute: 'name')
+                ->preload()
+                ->native(false)
+                ->searchable(),
+            Select::make('lesson_id')->label("Pelajaran")
+                ->required()
+                ->relationship(name: 'lesson', titleAttribute: 'name')
+                ->preload()
+                ->native(false)
+                ->searchable(),
+            Select::make('teacher_id')->label("Pengajar")
+                ->required()
+                ->options($teacher_options)
+                ->native(false)
+                ->searchable()
+                ->getSearchResultsUsing(
+                    function (string $search) use ($teacher_options): array {
+                        if ($search == '') return $teacher_options;
+                        return $teacher_options
+                            ->filter(function ($item) use ($search) {
+                                return false !== stripos($item, $search);
+                            })
+                            ->toArray();
+                    }
+                )
+                ->getOptionLabelUsing(
+                    function ($value) use ($teacher_options): string {
+                        return $teacher_options[$value];
+                    }
+                ),
+        ];
+    }
+
+    private function getFormEdit(): array
+    {
+        return [];
+    }
+}
