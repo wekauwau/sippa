6c2a7af3b07946f949d2e8ad2b1d813b2adb5f9e
wekauwau <wekauwau@gmail.com>
2024-06-14 22:03:55 +0700

    fix(route:madin-data): Semester column can't search "Ganjil"/"Genap"

--- app/Traits/MadinDataTable/Columns.php
+++ app/Traits/MadinDataTable/Columns.php
@@ -13,14 +13,9 @@ trait Columns
             TextColumn::make('semester.full_name')->label("Semester")
                 ->searchable(
                     query: function (Builder $query, string $search): Builder {
-                        // FIX: can't search "genap" or "ganjil"
-                        // because db is:
-                        //     boolean('isEven')
-                        // instead of:
-                        //     string('<name>') = "Ganjil", "Genap"
-                        // or there is a way? using collection?
                         return $query->whereRelation('semester', 'start', 'like', "%{$search}%")
-                            ->orWhereRelation('semester', 'end', 'like', "%{$search}%");
+                            ->orWhereRelation('semester', 'end', 'like', "%{$search}%")
+                            ->orWhereRelation('semester', 'semester', 'like', "%{$search}%");
                     },
                     isIndividual: true
                 ),
